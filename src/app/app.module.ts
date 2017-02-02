a906d0c52613f4805dc936156c420fab151a2638
Sara Jensen <jensen.sara.e@gmail.com>
2017-02-01 22:22:22 -0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EventFormComponent } from './event-form/event-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventFormComponent
   ],
   imports: [
     BrowserModule,
